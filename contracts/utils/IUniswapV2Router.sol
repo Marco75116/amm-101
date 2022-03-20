243ca401d69edb27afde7a707c2855c5fa0c3ded
Marc-Antoine Ourradour <83420479+Marco75116@users.noreply.github.com>
2022-03-20 23:32:29 +0100

    Add files via upload

---
+++ contracts/utils/IUniswapV2Router.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.6.0;
+
+interface IUniswapV2Router {
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function addLiquidityETH(
+        address token,
+        uint256 amountTokenDesired,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    )
+        external
+        payable
+        returns (
+            uint256 amountToken,
+            uint256 amountETH,
+            uint256 liquidity
+        );
+}
